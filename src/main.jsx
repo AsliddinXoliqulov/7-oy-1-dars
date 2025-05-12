139594d126379abded0801599fa50d195fcc59e5
Asliddin Xoliqulov <xasliddin04@gmail.com>
2025-05-12 18:47:41 +0500

    Initial commi

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import store from './redux/store';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client"; // ← React 18 uchun
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./redux/store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from './todoSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
 });
+
+export default store; // ✅ shu qatordan so‘ng main.jsx da `import store` ishlaydi
